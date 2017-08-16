b0f3e25ba92cd6d672514dbf6b7cd448f998a92e
Chewwy <31019539+Chewwy@users.noreply.github.com>
2017-08-15 21:10:41 -0700

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 
 client.on('ready', () => {
+  client.user.setGame('Lythrio | Do ~>help.');
   console.log('Bot is activated, and ready for action!');
 })
 
@@ -53,6 +54,37 @@ client.on('message', message => {
     .addField("Lythrio", "Use ~>lythrio to get information about the bot. (Warning, the bot can be sarcastic. Use at your own risk!)")
     .addField("Stupid", "Use ~>stupid to tell the bot what you really think about it! (Cannot guarantee it's not going to say a sarcastic answer back to you.)")
     .addField("Help", "Use ~>help to get a list of information for the server. Including the Server IP, Website information, and much, much more. It will also tell you if the server is online or not!")
+    .addField("Server Status", "Use ~>serverstatus it tells you if the server is up or down, if the server is down, it'll tell you why, and when it'll be back up")
+    .addField("Server Information", "Use ~>server information it tells you a description of the server, the server IP, and much much more.")
+    .addField("Website", "Tells you information about the website, and if it's down or not. It will tell you where you can apply, and important links.")
+  message.channel.send({embed});
+  }
+  if (command === "serverstatus" || command === "Serverstatus"){
+    const embed = new Discord.RichEmbed()
+    .setFooter("Server Status | Lythrio")
+    .setTitle("")
+    .addField("Server Status", "Currently, the server is offline.")
+    .addField("Reason:", "The server is still in development mode. Try again later.")
+    .addField("Fix Date:", "The server should be ready by 9/15/17.")
+  message.channel.send({embed});
+  }
+  if (command === "serverinformation" || command === "Serverinformation"){
+    const embed = new Discord.RichEmbed()
+    .setFooter("Server Information | Lythrio")
+    .setTitle("")
+    .addField("Server Description:", "Lythrio is a server that strives on providing the best survival gameplay. With our experienced staff, and server operators, you can be glad to join the server, time after time.")
+    .addField("Server Version:", "Currently we are on 1.12.1, we will update when Minecraft updates. (There may be a small downtime when this happens.")
+    .addField("Server IP", "Currently there is no IP, but we will list one here when there is one. (We release on 9/15/17).")
+    .addField("Need Help?", "Do you need help? You may contact anyone on the staff team, or Chewwy/Lasantha and we will help you, or get the problem solved ASAP.")
+  message.channel.send({embed});
+  }
+  if (command === "website" || command === "Website"){    
+    const embed = new Discord.RichEmbed()
+    .setFooter("Website | Lythrio")
+    .setTitle("")
+    .addField("Information" , "Hello, currently the server website is still in development! (We will update this message when it is developed, and everything is ready to go.)")
+    .addField("Website:", "Currently the website is being developed")
+    .addField("Website Information", "The website is being developed, and is going to be amazing. There will be room for applications for staff, builders and developers. If you'd like to apply, please go to our website, and do so there!")
   message.channel.send({embed});
   }
 });
